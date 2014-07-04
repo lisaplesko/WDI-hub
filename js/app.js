6a77545a54030c99e9e3d0f407f71d40a70b6092
Lisa Plesko <plesko.l@gmail.com>
2014-07-04 13:28:37 -0400

    Add cacheFactory; need to test

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 'use strict';
 
-var gaApp = angular.module('gaApp', ['ngAnimate']);
+var gaApp = angular.module('gaApp', []);
+// gaApp.service('studentCache', function())
+// gaApp.factory('studentCache', ['$cacheFactory', function ($cacheFactory) {
+//   return $cacheFactory('studentData');
+// }]);
 
-gaApp.controller('StudentsCtrl', ['$scope', '$http', function ($scope, $http) {
+gaApp.controller('StudentsCtrl', ['$scope', '$http', '$cacheFactory', function ($scope, $http, $cacheFactory) {
 
   $scope.students = [];
   $http({
     url: 'http://localhost:3000/students',
     method: 'GET',
+    cache: true
   }).success(function (students){
     $scope.students = students;
     $scope.studentsCount = $scope.students.length;
