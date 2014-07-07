d39986faffce1de1df4b6444c7a4afe217af8f3b
Lisa Plesko <plesko.l@gmail.com>
2014-07-07 15:20:08 -0400

    Change repo serializer to 'updated_at'; need to fix rake query to retrieve date

--- github_api/app/serializers/repo_serializer.rb
+++ github_api/app/serializers/repo_serializer.rb
@@ -1,4 +1,4 @@
 class RepoSerializer < ActiveModel::Serializer
-  attributes :name, :stargazers_count, :watchers_count, :description, :html_url, :created_at
+  attributes :name, :stargazers_count, :watchers_count, :description, :html_url, :updated_at
   has_many :languages
 end
