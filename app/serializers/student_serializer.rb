eb7cae04fd1653c515e2f0527e7558f16901d0a5
Lisa Plesko <plesko.l@gmail.com>
2014-07-06 21:52:16 -0400

    Add :url to student serializer

--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -1,4 +1,4 @@
 class StudentSerializer < ActiveModel::Serializer
-  attributes :id, :firstname, :lastname, :username, :avatar_url, :city, :session, :followers, :following, :public_repos, :blog
+  attributes :id, :firstname, :lastname, :username, :avatar_url, :url, :city, :session, :followers, :following, :public_repos, :blog
   has_many :events
 end
