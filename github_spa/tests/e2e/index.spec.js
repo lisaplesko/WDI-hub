39a6b24c1b196a32f8c93a002ce4e305f5ac6d2b
Lisa Plesko <plesko.l@gmail.com>
2014-07-09 11:20:04 -0400

    Rewrite test specs to access elements through page, not through test

--- github_spa/tests/e2e/index.spec.js
+++ github_spa/tests/e2e/index.spec.js
@@ -1,7 +1,7 @@
 function IndexPage() {
-  this.buttonNYC = element(by.id('NYC'));
-  this.buttonBoston = element(by.id('Boston'));
-  this.buttonAll = element(by.id('all'));
+
+  this.studentList = element.all(by.repeater('student in students'));
+  this.query = element(by.model('search.firstname'));
 
   this.get = function() {
     browser.get('http://localhost:5000');
@@ -10,9 +10,15 @@ function IndexPage() {
   this.getTitle = function() {
     return browser.getTitle();
   }
+
+  this.clickButton = function(id) {
+    var button = element(by.id(id));
+    button.click();
+  }
+
 }
 
-describe('angularjs homepage', function() {
+describe('gaApp homepage', function() {
 
   var page = new IndexPage();
 
@@ -29,39 +35,32 @@ describe('angularjs homepage', function() {
 
   it('should filter the student list as user types into the search box', function(){
 
-    var studentList = element.all(by.repeater('student in students'));
-    var query = element(by.model('search.firstname'));
+    expect(page.studentList.count()).toBe(53);
 
-    expect(studentList.count()).toBe(53);
+    page.query.sendKeys('Lisa');
+    expect(page.studentList.count()).toBe(1);
 
-    query.sendKeys('Lisa');
-    expect(studentList.count()).toBe(1);
+    page.query.clear();
+    page.query.sendKeys('Emma');
+    expect(page.studentList.count()).toBe(2);
 
-    query.clear();
-    query.sendKeys('Emma');
-    expect(studentList.count()).toBe(2);
-
-    query.clear();
-    query.sendKeys('Lisa');
-    expect(element(by.model('search.firstname')).getAttribute('value')).toEqual('Lisa');
+    page.query.clear();
+    page.query.sendKeys('Lisa');
+    expect((page.query).getAttribute('value')).toEqual('Lisa');
 
     // expect(element(by.binding('student.firstname')).getText()).toEqual('Lisa');
   });
 
   it('should filter the student list when a city button is clicked', function(){
-    var buttonNYC = element(by.id('NYC')),
-        buttonBoston = element(by.id('Boston')),
-        buttonAll = element(by.id('all')),
-        studentList = element.all(by.repeater('student in students'));
 
-    buttonNYC.click();
-    expect(studentList.count()).toBe(5);
+    page.clickButton('NYC');
+    expect(page.studentList.count()).toBe(5);
 
-    buttonBoston.click();
-    expect(studentList.count()).toBe(48);
+    page.clickButton('Boston');
+    expect(page.studentList.count()).toBe(48);
 
-    buttonAll.click();
-    expect(studentList.count()).toBe(53);
+    page.clickButton('all');
+    expect(page.studentList.count()).toBe(53);
 
   });
 
