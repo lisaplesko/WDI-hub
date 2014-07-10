390d62013e2509298355ed8c8caff2ff05ed568d
Lisa Plesko <plesko.l@gmail.com>
2014-07-10 10:46:53 -0400

    Add cloudfront to production environment

--- github_api/config/environments/production.rb
+++ github_api/config/environments/production.rb
@@ -55,6 +55,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "d2knsmfb849ehs.cloudfront.net"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
