448865f55107d5e80f803c326b36c373a193c33a
Lisa Plesko <plesko.l@gmail.com>
2014-07-12 15:02:12 -0400

    Removed unused methods in students#controller, removed root to in routes

--- github_api/app/controllers/students_controller.rb
+++ github_api/app/controllers/students_controller.rb
@@ -9,14 +9,11 @@ class StudentsController < ApplicationController
     render json: @students
   end
 
-  # GET /students/1
-  # GET /students/1.json
   def show
     @student = Student.find(params[:id])
 
     render json: @student, serializer: StudentShowSerializer
   end
-
 
   def total_code
     student = Student.find(params[:id])
@@ -30,39 +27,6 @@ class StudentsController < ApplicationController
     @student_languages = student.lines_per_language
 
     render json: @student_languages
-  end
-
-  # POST /students
-  # POST /students.json
-  def create
-    @student = Student.new(student_params)
-
-    if @student.save
-      render json: @student, status: :created, location: @student
-    else
-      render json: @student.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /students/1
-  # PATCH/PUT /students/1.json
-  def update
-    @student = Student.find(student_params[:id])
-
-    if @student.update(student_params[:student])
-      head :no_content
-    else
-      render json: @student.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /students/1
-  # DELETE /students/1.json
-  def destroy
-    @student = Student.find(params[:id])
-    @student.destroy
-
-    head :no_content
   end
 
   private
--- github_api/config/routes.rb
+++ github_api/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'students#index'
 
   get 'students/:id/total_code', :to => 'students#total_code'
   get 'students/:id/total_language', :to => 'students#total_language'
