c7b568396b9fd00586dd906eb2788f970452faa5
Lisa Plesko <plesko.l@gmail.com>
2014-07-12 16:38:19 -0400

    Edit student serializer to only send top 3 favorite repos

--- github_api/app/serializers/student_show_serializer.rb
+++ github_api/app/serializers/student_show_serializer.rb
@@ -2,4 +2,8 @@ class StudentShowSerializer < ActiveModel::Serializer
   attributes :id, :firstname, :lastname, :username, :avatar_url, :url, :city, :followers, :following, :public_repos, :blog
   has_many :events
   has_many :repos
+
+  def repos
+    Repo.where(student: id).order("stargazers_count").last(3)
+  end
 end
