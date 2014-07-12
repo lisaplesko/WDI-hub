064783590e04dd4202ac7ec0dba57d6a94779ac1
Lisa Plesko <plesko.l@gmail.com>
2014-07-12 12:28:13 -0400

    Remove :watchers_count and :session from serializer; not currently being used

--- github_api/app/serializers/repo_serializer.rb
+++ github_api/app/serializers/repo_serializer.rb
@@ -1,4 +1,4 @@
 class RepoSerializer < ActiveModel::Serializer
-  attributes :name, :stargazers_count, :watchers_count, :description, :html_url, :updated, :homepage
+  attributes :name, :stargazers_count, :description, :html_url, :updated, :homepage
   has_many :languages
 end
--- github_api/app/serializers/student_show_serializer.rb
+++ github_api/app/serializers/student_show_serializer.rb
@@ -1,5 +1,5 @@
 class StudentShowSerializer < ActiveModel::Serializer
-  attributes :id, :firstname, :lastname, :username, :avatar_url, :url, :city, :session, :followers, :following, :public_repos, :blog
+  attributes :id, :firstname, :lastname, :username, :avatar_url, :url, :city, :followers, :following, :public_repos, :blog
   has_many :events
   has_many :repos
 end
